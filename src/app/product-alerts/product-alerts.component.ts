d81fec5408a0d000ae226d5b33de69ed17a5019e
lit-john <john.hannafin@lit.ie>
2019-11-29 19:08:02 +0000

    Completed the product alerts component

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,7 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
-// Import Input
+// Import Input as this component needs to accept an input from
+// a parent component
 import { Input } from '@angular/core';
+
+// Import Output as this component needs to output (return) something
+// to it's parent component. Import EventEmitter as this is what we
+// are going to output
+import { Output, EventEmitter} from '@angular/core';
 
 // Below is the @Component decorator
 @Component({
@@ -13,6 +19,11 @@ export class ProductAlertsComponent implements OnInit {
   // The @Input decorator indicates that the property value passes in from
   // the component's parent, the product list component
   @Input() product;
+
+  // Create an EventEmitter object called notify which we will use as
+  // an output to the parent component. The name of this property 
+  // i.e. 'notify' will be used by the parent component to bind to.
+  @Output() notify = new EventEmitter();
   constructor() { }
 
   ngOnInit() {
