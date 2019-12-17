878f293acba949fc35ffa482fcd64ff46decb130
lit-john <john.hannafin@lit.ie>
2019-12-17 15:55:00 +0000

    Added a form to the cart

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+
+/*
+ We are going to use Angular's FormBuilder service so let's import it and we'll then inject it into the comnponent.
+ */
+import { FormBuilder, FormGroup } from '@angular/forms';
+
 import { CartService } from '../cart.service';
 
 
@@ -8,14 +14,32 @@ import { CartService } from '../cart.service';
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent implements OnInit {
-  public items;any;
+  public items:any;
+
+  // The checkOutForm variable will hold our form model
+  public checkoutForm:FormGroup;
   /*
-  Inject the CartService and save it as a private instance variable
+  Inject the CartService and FormBuilder and save them as private instance variables
   */
-  constructor(private cartService: CartService) { }
+  constructor(
+    private cartService: CartService,
+    private formBuilder: FormBuilder) {
+     }
 
   ngOnInit() {
     this.items = this.cartService.getItems();
+
+      this.checkoutForm = this.formBuilder.group({
+        name: '',
+        address: ''
+    });
+  }
+
+  onSubmit(customerData) {
+    console.warn("Your order has been submitted", customerData);
+
+    this.items = this.cartService.clearCart();
+    this.checkoutForm.reset();
   }
 
 }
